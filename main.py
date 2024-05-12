ec6e3d902161eae737c9132a32bcabb03747a81d
David Bakalarczyk <david.bakalarczyk00@gmail.com>
2024-05-12 13:19:31 +0200

    Python Edit: Create routes for web header

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/about_me')
+def about_me():
+    return render_template('about_me.html')
+
+@app.route('/say_hello')
+def say_hello():
+    return render_template('say_hello.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
