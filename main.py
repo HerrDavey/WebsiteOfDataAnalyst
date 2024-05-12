f736570a876cfa18a4df06142e9cf5a901e29951
David Bakalarczyk <david.bakalarczyk00@gmail.com>
2024-05-12 14:44:18 +0200

    Create structure for database in Python

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
+#IMPORTS 
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
+
+#DATABASE DECLARETION
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+
+#APP AND SQL CONFIG
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+db.init_app(app)
 
+#DATABASE
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str]
+
+with app.app_context():
+    db.create_all()
+
+#ROUTES
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -14,5 +39,8 @@ def about_me():
 def say_hello():
     return render_template('say_hello.html')
 
+
+
+#INITIALISATION
 if __name__ == '__main__':
     app.run(debug=True)
