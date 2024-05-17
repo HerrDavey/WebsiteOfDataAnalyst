57e7e0ddffb0be295a3868ae037fe9d9b804627c
David Bakalarczyk <david.bakalarczyk00@gmail.com>
2024-05-17 16:47:54 +0200

    Visual correction of contact form

--- forms.py
+++ forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import  StringField, SubmitField, validators
+from wtforms import  StringField, SubmitField, TextAreaField, validators
 from wtforms.validators import DataRequired, Email, URL
 import email_validator
 
 class SayHello(FlaskForm): 
     name = StringField(label='Name', validators=[DataRequired()])
     email = StringField(label='Email', validators=[DataRequired(), Email(granular_message=True)])
-    message = StringField(label='Your Message')
+    message = TextAreaField(label='Your Message')
     submit = SubmitField(label="Sent Message")
