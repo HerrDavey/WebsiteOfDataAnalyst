4c292f773d03b283ed4d6b170c07364b90fe3b58
David Bakalarczyk <david.bakalarczyk00@gmail.com>
2024-05-24 13:48:35 +0200

    Footer: Add links to profiles

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #IMPORTS 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -7,6 +7,7 @@ from forms import SayHello
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+import smtplib, ssl
 
 #DATABASE DECLARETION
 class Base(DeclarativeBase):
@@ -45,6 +46,21 @@ def about_me():
 @app.route('/say_hello', methods=["GET", "POST"])
 def say_hello():
     cform = SayHello()
+
+    #Email send config
+    port = 465
+    smtp_server = "smtp.gmail.com"
+    receiver_email = "david.bakalarczyk00@gmail.com"  
+  
+    if cform.validate_on_submit():
+        name_sender = cform.name.data
+        email_sender = cform.email.data
+        message_sender = cform.message.data
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port=port, context=context) as server:
+            server.sendmail(email_sender, receiver_email, message_sender)
+        return redirect(url_for('say_hello'))
     return render_template('say_hello.html', form=cform)
 
 
